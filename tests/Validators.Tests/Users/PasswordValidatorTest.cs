02a295e1e59b79b2ea2b4fa31c744b678bc27df6
Davi Freitas <davi.frrs@outlook.com>
2024-07-03 09:44:32 -0300

    test: Password validator

---
+++ tests/Validators.Tests/Users/PasswordValidatorTest.cs
@@ -0,0 +1,39 @@
+﻿using CashFlow.Application.UseCases.Users;
+using CashFlow.Communication.Requests.Auth;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using FluentValidation;
+
+namespace Validators.Tests.Users
+{
+    public class PasswordValidatorTest
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("  ")]
+        [InlineData(null)]
+        [InlineData("a")]
+        [InlineData("aa")]
+        [InlineData("aaa")]
+        [InlineData("aaaa")]
+        [InlineData("aaaaa")]
+        [InlineData("aaaaaa")]
+        [InlineData("aaaaaaa")]
+        [InlineData("aaaaaaaa")]
+        [InlineData("AAAAAAAA")]
+        [InlineData("Aaaaaaaa")]
+        [InlineData("Aaaaaaaa1")]
+        public void ErrorInvalidPassword(string password)
+        {
+            // Arrange
+            var validator = new PasswordValidator<RequestRegisterUserJson>();
+
+            // Act
+            var result = validator
+                .IsValid(new ValidationContext<RequestRegisterUserJson>(new RequestRegisterUserJson()), password);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+    }
+}
