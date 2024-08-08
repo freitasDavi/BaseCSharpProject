85ce7f98478fbfa1a957bbc7c5cf1f146f54f747
Davi Freitas <davi.frrs@outlook.com>
2024-08-08 11:04:09 -0300

    feat: Creating tags migrations

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -8,7 +8,15 @@ namespace CashFlow.Infrastructure.DataAccess
         public DbSet<Expense> Expenses { get; set; }
         public DbSet<User> Users { get; set; }  
         public DbSet<Income> Incomes { get; set; }
+        
 
         public CashFlowDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Tag>().ToTable("Tags");
+        }
     }
 }
